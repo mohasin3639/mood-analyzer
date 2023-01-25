21179bc8b256743536e1971fe38f30e802d017f1
mohasin3639 <mohasinsayyad3639@gmail.com>
2023-01-25 17:43:55 +0530

    ADDED:Welcome Message

---
+++ src/main/java/com/bridgelabz/mood_analyser/MoodAnalyzer.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.mood_analyser;
+
+public class MoodAnalyzer {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Mood Analyzer Program");
+    }
+}
